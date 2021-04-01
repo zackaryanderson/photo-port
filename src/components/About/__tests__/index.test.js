268f2f44fc0b0dd0d6763862ae5657ff880bc39d
zanderson1998 <zanderson1998@yahoo.com>
2021-04-01 17:21:45 -0600

    add tests for About component

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {render, cleanup} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //renders about test
+    //first test
+    it('renders', () => {
+        render(<About />);
+    });
+
+    //second test
+    it('matches snapshot DOM node structure', () => {
+        const {asFragment} = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -21,7 +21,7 @@ function Nav() {
     }
 
     return (
-        <header>
+        <header data-testid="header" className="flex-row px-1">
             <h2>
                 <a href="/">
                     <span role="img" aria-label="camera">📸</span> Oh Snap?
