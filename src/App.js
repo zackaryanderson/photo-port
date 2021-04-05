54e3afb53bb2f6c0ec3e11178c6a29392073e606
zanderson1998 <zanderson1998@yahoo.com>
2021-04-05 10:12:01 -0600

    add form submit options and form capture ability

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
 
@@ -30,6 +31,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
